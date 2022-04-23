eab46bfc9e1995398df40b23138c807f8bf59501
aliceyzhou <aliceyzh@usc.edu>
2022-04-23 16:07:14 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import { useEffect, useRef } from 'react';
+import socketIOClient from 'socket.io-client';
+
+const App = () => {
+  const GET_CODE = 'getCode';
+  const SOCKET_SERVER_URL = 'https://trojanhacks.herokuapp.com/'; //'http://localhost:4000';
+
+  const [p1code, setp1code] = useState('hello world');
+  const [p2code, setp2code] = useState('');
+
+  const sendCode = (messageBody) => {
+    socketRef.current.emit(GET_CODE, {
+      body: messageBody,
+      senderId: socketRef.current.id,
+    });
+  };
+
+  const pressSubmit1 = () => {
+    setp1code('pressed 1');
+    sendCode('hello');
+  };
+  const getP1Code = (e) => {
+    setp1code(e.target.value);
+  };
+  const pressSubmit2 = () => {
+    setp2code('pressed 2');
+  };
+  const getP2Code = (e) => {
+    setp1code(e.target.value);
+  };
+
+  const socketRef = useRef();
+
+  useEffect(() => {
+    // Creates a WebSocket connection
+    socketRef.current = socketIOClient(SOCKET_SERVER_URL);
+    // Listens for incoming messages
+    socketRef.current.on(GET_CODE, (data) => {
+      console.log(data);
+      //setMessages((messages) => [...messages, incomingMessage]);
+    });
+
+    // Destroys the socket reference
+    // when the connection is closed
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='problem'>The problem placeholder text here</div>
+
+      <div className='container'>
+        <div className='player'>
+          <select>
+            <option value='Python'>Python</option>
+            <option value='C++'>C++</option>
+          </select>
+
+          <textarea
+            className='code'
+            placeholder='begin coding here...'
+            onChange={getP1Code}
+          ></textarea>
+          <button class='submit' onClick={pressSubmit1}>
+            Submit!
+          </button>
+
+          <textarea className='output' placeholder='output'></textarea>
+        </div>
+
+        <div className='player'>
+          <select>
+            <option value='Python'>Python</option>
+            <option value='C++'>C++</option>
+          </select>
+          <textarea
+            className='code'
+            placeholder='begin coding here...'
+            onChange={getP2Code}
+          ></textarea>
+          <button class='submit' onClick={pressSubmit2}>
+            Submit!
+          </button>
+
+          <textarea className='output' placeholder='output'></textarea>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
